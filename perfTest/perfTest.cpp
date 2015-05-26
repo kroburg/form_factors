f63e9e11da88660b60d3089b7fb87a0bdd81f94f
Stepan Tezyunichev <stepan.tezyunichev@gmail.com>
2015-05-26 22:15:47 +0300

    fixed: don't crash on zero rays count

--- perfTest/perfTest.cpp
+++ perfTest/perfTest.cpp
@@ -93,6 +93,8 @@ scene_t* MakeConfettiScene(int n_faces, ShperePointsGenerator& generator)
 task_t* MakeCollapsingRays(int n_rays, ShperePointsGenerator& generator)
 {
   math::ray_t* rays = (math::ray_t*)malloc(n_rays * sizeof(math::ray_t));
+  if (rays == 0)
+    throw std::out_of_range("not enough memory to allocate rays");
   face_t** hit_face = (face_t**)malloc(n_rays * sizeof(face_t*));
   math::vec3* hit_point = (math::vec3*)malloc(n_rays * sizeof(math::vec3));
   task_t* task = (task_t*)malloc(sizeof(task_t));
@@ -176,107 +178,115 @@ int CalculateMismatch(task_t* cpu, task_t* gpu)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-  int n_faces = 1000;
-  int n_rays = 10000 * n_faces;
-  bool no_cpu = true;
-  bool no_form_factors = false;
-
-  ShperePointsGenerator generator;
-  system_t* cuda_system = system_create(RAY_CASTER_SYSTEM_CUDA);
-  system_t* cpu_system = system_create(RAY_CASTER_SYSTEM_CPU);
-  printf("Generating confetti scene with %d elements...\n", n_faces);
-  scene_t* scene = MakeConfettiScene(n_faces, generator);
-  printf("Generating %d collapsing rays...\n", n_rays);
-  task_t* gpuTask = MakeCollapsingRays(n_rays, generator);
-  task_t* cpuTask = task_clone(gpuTask);
-
-  printf("Warming up...\n");
-  scene_t warm_up_scene = *scene;
-  warm_up_scene.n_faces = warm_up_scene.n_faces > 32 ? 32 : warm_up_scene.n_faces;
-  task_t warm_up_task = *gpuTask;
-  warm_up_task.n_tasks = 1;
-  system_set_scene(cuda_system, &warm_up_scene);
-  system_prepare(cuda_system);
-  system_cast(cuda_system, &warm_up_task);
-
-  system_set_scene(cuda_system, scene);
-  system_prepare(cuda_system);
-  system_set_scene(cpu_system, scene);
-  system_prepare(cpu_system);
-
-  StopWatchInterface *hTimer;
-  sdkCreateTimer(&hTimer);
-  sdkResetTimer(&hTimer);
-  sdkStartTimer(&hTimer);
-
-  printf("Casting scene on GPU...\n");
-  system_cast(cuda_system, gpuTask);
-  sdkStopTimer(&hTimer);
-  double gpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
-  printf("Done in %fs.\n", gpuTime);
-
-  if (!no_cpu)
+  try
   {
-    printf("Casting scene on CPU...\n");
+    int n_faces = 1000;
+    int n_rays = 1000 * n_faces;
+    bool no_cpu = true;
+    bool no_form_factors = false;
+
+    ShperePointsGenerator generator;
+    system_t* cuda_system = system_create(RAY_CASTER_SYSTEM_CUDA);
+    system_t* cpu_system = system_create(RAY_CASTER_SYSTEM_CPU);
+    printf("Generating confetti scene with %d elements...\n", n_faces);
+    scene_t* scene = MakeConfettiScene(n_faces, generator);
+    printf("Generating %d collapsing rays...\n", n_rays);
+    task_t* gpuTask = MakeCollapsingRays(n_rays, generator);
+    task_t* cpuTask = task_clone(gpuTask);
+
+    printf("Warming up...\n");
+    scene_t warm_up_scene = *scene;
+    warm_up_scene.n_faces = warm_up_scene.n_faces > 32 ? 32 : warm_up_scene.n_faces;
+    task_t warm_up_task = *gpuTask;
+    warm_up_task.n_tasks = 1;
+    system_set_scene(cuda_system, &warm_up_scene);
+    system_prepare(cuda_system);
+    system_cast(cuda_system, &warm_up_task);
+
+    system_set_scene(cuda_system, scene);
+    system_prepare(cuda_system);
+    system_set_scene(cpu_system, scene);
+    system_prepare(cpu_system);
+
+    StopWatchInterface *hTimer;
+    sdkCreateTimer(&hTimer);
     sdkResetTimer(&hTimer);
     sdkStartTimer(&hTimer);
-    system_cast(cpu_system, cpuTask);
+
+    printf("Casting scene on GPU...\n");
+    system_cast(cuda_system, gpuTask);
     sdkStopTimer(&hTimer);
-    double cpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
-    printf("Done in %fs.\n", cpuTime);
+    double gpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
+    printf("Done in %fs.\n", gpuTime);
 
-    ErrorStats error = CalculateError(cpuTask, gpuTask);
-    printf("Error is %f average, %f max, %f total.\n", error.AverageDistance, error.MaxDistance, error.TotalDistance);
-    printf("Face hit mismatch count is %d.\n", error.Mismatch);
-    printf("GPU/CPU performance ratio is %f.\n", cpuTime / gpuTime);
-  }
-
-  if (!no_form_factors)
-  {
-    form_factors::system_t* calculator = form_factors::system_create(FORM_FACTORS_CPU, cuda_system);
-    form_factors::scene_t calculator_scene;
-    calculator_scene.n_faces = scene->n_faces;
-    calculator_scene.faces = scene->faces;
-
-    int n_meshes = scene->n_faces;
-    calculator_scene.n_meshes = n_meshes;
-    calculator_scene.meshes = (form_factors::mesh_t*) malloc(n_meshes * sizeof(form_factors::mesh_t));
-    for (int i = 0; i != n_meshes; ++i)
+    if (!no_cpu)
     {
-      form_factors::mesh_t& mesh = calculator_scene.meshes[i];
-      mesh.first_idx = i;
-      mesh.n_faces = 1;
+      printf("Casting scene on CPU...\n");
+      sdkResetTimer(&hTimer);
+      sdkStartTimer(&hTimer);
+      system_cast(cpu_system, cpuTask);
+      sdkStopTimer(&hTimer);
+      double cpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
+      printf("Done in %fs.\n", cpuTime);
+
+      ErrorStats error = CalculateError(cpuTask, gpuTask);
+      printf("Error is %f average, %f max, %f total.\n", error.AverageDistance, error.MaxDistance, error.TotalDistance);
+      printf("Face hit mismatch count is %d.\n", error.Mismatch);
+      printf("GPU/CPU performance ratio is %f.\n", cpuTime / gpuTime);
     }
 
-    printf("Calculating form factors on CPU...\n");
-    sdkResetTimer(&hTimer);
-    sdkStartTimer(&hTimer);
+    if (!no_form_factors)
+    {
+      form_factors::system_t* calculator = form_factors::system_create(FORM_FACTORS_CPU, cuda_system);
+      form_factors::scene_t calculator_scene;
+      calculator_scene.n_faces = scene->n_faces;
+      calculator_scene.faces = scene->faces;
 
-    form_factors::system_set_scene(calculator, &calculator_scene);
-    form_factors::system_prepare(calculator);
-    form_factors::task_t* task = form_factors::task_create(&calculator_scene, n_rays);
-    form_factors::system_calculate(calculator, task);
+      int n_meshes = scene->n_faces;
+      calculator_scene.n_meshes = n_meshes;
+      calculator_scene.meshes = (form_factors::mesh_t*) malloc(n_meshes * sizeof(form_factors::mesh_t));
+      for (int i = 0; i != n_meshes; ++i)
+      {
+        form_factors::mesh_t& mesh = calculator_scene.meshes[i];
+        mesh.first_idx = i;
+        mesh.n_faces = 1;
+      }
 
-    sdkStopTimer(&hTimer);
-    double cpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
-    printf("Done in %fs.\n", cpuTime);
+      printf("Calculating form factors on CPU...\n");
+      sdkResetTimer(&hTimer);
+      sdkStartTimer(&hTimer);
 
-    task_free(task);
-    free(calculator_scene.meshes);
+      form_factors::system_set_scene(calculator, &calculator_scene);
+      form_factors::system_prepare(calculator);
+      form_factors::task_t* task = form_factors::task_create(&calculator_scene, n_rays);
+      form_factors::system_calculate(calculator, task);
+
+      sdkStopTimer(&hTimer);
+      double cpuTime = 1.0e-3 * sdkGetTimerValue(&hTimer);
+      printf("Done in %fs.\n", cpuTime);
+
+      task_free(task);
+      free(calculator_scene.meshes);
+    }
+
+    system_free(cuda_system);
+    system_free(cpu_system);
+
+    scene_free(scene);
+    free(gpuTask->ray);
+    free(gpuTask->hit_face);
+    free(gpuTask->hit_point);
+    free(gpuTask);
+    free(cpuTask->hit_face);
+    free(cpuTask->hit_point);
+    free(cpuTask);
+
+    return 0;
   }
-  
-  system_free(cuda_system);
-  system_free(cpu_system);
-
-  scene_free(scene);
-  free(gpuTask->ray);
-  free(gpuTask->hit_face);
-  free(gpuTask->hit_point);
-  free(gpuTask);
-  free(cpuTask->hit_face);
-  free(cpuTask->hit_point);
-  free(cpuTask);
-
-	return 0;
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+    return -1;
+  }
 }
 
