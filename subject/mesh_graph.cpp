aa5bcff9208667d071b0f0bbfe859d81f78e4bab
Stepan Tezyunichev <stepan.tezyunichev@gmail.com>
2015-09-28 10:41:25 +0300

    new: Implementing indexed face graph walker

--- subject/mesh_graph.cpp
+++ subject/mesh_graph.cpp
@@ -22,6 +22,12 @@
 #include "../math/operations.h"
 #include "../math/triangle.h"
 #include <cmath>
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <set>
+#include <vector>
+#include <assert.h>
 
 namespace subject
 {
@@ -58,32 +64,109 @@ namespace subject
     return 0;
   }
 
-  struct buffered_face_point
+  struct indexed_walker
   {
-    math::vec3 point;
-
-    bool operator < (const buffered_face_point& r) const
+    struct face_point
     {
-      const math::vec3 size = max(abs(point), abs(r.point));
-      math::vec3 diff = (point - r.point) / size;
-      if (diff.x < -FLT_EPSILON)
-        return true;
-      else if (diff.x > FLT_EPSILON)
-        return false;
-      else if (diff.y < -FLT_EPSILON)
-        return true;
-      else if (diff.y > FLT_EPSILON)
-        return false;
-      else if (diff.z < -FLT_EPSILON)
-        return true;
+      int point;
+      char index;
+      bool operator<(const face_point& r) const
+      {
+        return point < r.point;
+      }
+    };
+
+    struct indexed_face_t
+    {
+      int points[3];
+    };
+
+    typedef std::map<math::vec3, int> point_index;
+    typedef std::set<face_point> united_faces;
+    typedef std::map<int, united_faces> point_face_index;
+
+    point_index points;
+    std::vector<indexed_face_t> indexed_faces;
+    point_face_index p2f;
+
+    int index_point(math::vec3 p)
+    {
+      typedef std::pair<point_index::iterator, bool> point_insertion;
+      point_insertion r = points.insert(point_index::value_type(p, 0));
+      if (r.second)
+        r.first->second = (int)points.size();
+      return r.first->second;
+    }
+
+    int face_edge(int f_idx, int p1, int p2)
+    {
+      const indexed_face_t&f = indexed_faces[f_idx];
+      if (f.points[0] == p1 || f.points[0] == p2)
+      {
+        if (f.points[1] == p1 || f.points[1] == p2)
+        {
+          assert(!(f.points[2] == p1 || f.points[2] == p2));
+          return 1 << 0;
+        }
+        else if (f.points[2] == p1 || f.points[2] == p2)
+        {
+          assert(!(f.points[1] == p1 || f.points[1] == p2));
+          return 1 << 2;
+        }
+        else
+        {
+          assert(!"Invalid face points");
+          return -1;
+        }
+      }
       else
-        return false;
+      {
+        assert(f.points[1] == p1 || f.points[1] == p2);
+        assert(f.points[2] == p1 || f.points[2] == p2);
+        return 1 << 1;
+      }
+    }
+
+    void collect_adjacents(int source, int p1, int p2)
+    {
+      const united_faces& f1 = p2f[p1];
+      const united_faces& f2 = p2f[p2];
+      std::vector<face_point> common;
+      std::set_intersection(f1.begin(), f1.end(), f2.begin(), f2.end(), std::back_inserter(common));
+      for (face_point f : common)
+      {
+        if (f.point != source);
+
+      }
+    }
+
+    int face_walk_graph_nlgn(const face_t* faces, int n_faces, face_graph_walker walker, void* param)
+    {
+      build_face_points_index(faces, n_faces);
+
+      for (int f = 0; f != n_faces; ++f)
+      {
+
+
+      }
+
+      return 0;
+    }
+
+    void build_face_points_index(const face_t* faces, int n_faces)
+    {
+      indexed_faces.resize(n_faces);
+
+      for (int f = 0; f != n_faces; ++f)
+      {
+        for (char p = 0; p != 3; ++p)
+        {
+          int point_idx = index_point(faces[f].points[p]);
+          indexed_faces[f].points[p] = point_idx;
+          face_point entry = { f, p };
+          p2f[point_idx].insert(entry);
+        }
+      }
     }
   };
-
-
-  int face_walk_graph_nlgn(const face_t* faces, int n_faces, face_graph_walker walker, void* param)
-  {
-    return 0;
-  }
 }
